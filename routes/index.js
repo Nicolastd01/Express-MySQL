fc49d37bef77f7ab616120ab02ea12254f23d7ee
Nicolas Teixeira <nicolasgames1570@gmail.com>
2023-06-21 13:03:30 -0300

    banco

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2/promise');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    mysql.createConnection({host: 'localhost',user: 'Nicolas', password: '!1570!',
-    database: 'espress-mysql',port: 3306
+    mysql.createConnection({host: 'localhost',user: 'root', password: '',
+    database: 'express-mysql',port: 3306
     }).then((connection) => {connection.query('select * from clientes;')
       .then((result) => {res.send(result[0]);});
     });
