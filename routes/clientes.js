3851462ef7d954501ef8b2fe12c3de3b243316e1
Nicolas Teixeira <nicolasgames1570@gmail.com>
2023-06-22 17:49:00 -0300

    modularizando o codigo

---
+++ routes/clientes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET clientes*/
+router.get('/', function(req, res, next){
+    res.send('GET clientes');
+});
+
+/* POST clientes*/
+router.post('/', function(req, res, next){
+    res.send('POST clientes');
+});
+
+/* PUT clientes*/
+router.put('/', function(req, res, next){
+    res.send('PUT clientes');
+});
+
+/* DELETE clentes*/
+router.delete('/', function(req, res, next){
+    res.send('DELETE clientes');
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,7 @@ const mysql = require('mysql2/promise');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    mysql.createConnection({host: 'localhost',user: 'root', password: '',
-    database: 'express-mysql',port: 3306
-    }).then((connection) => {connection.query('select * from clientes;')
+    ((connection) => {connection.query('select * from clientes;')
       .then((result) => {res.send(result[0]);});
     });
   
